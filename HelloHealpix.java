7ede7cddc11c115574a7ef841a8bfdef4df38957
ssbx <sebastien.serre@sysmo.io>
2017-11-28 13:54:03 +0100

    include cell 0

--- HelloHealpix.java
+++ HelloHealpix.java
@@ -5,7 +5,8 @@ import healpix.essentials.Scheme;
 public class HelloHealpix {
 	
     public static void main(String[] args) {
-	long nside = 2048;
+	long nside = 16;
+	long npix = 3072;
 	Scheme scheme = Scheme.NESTED;
 	HealpixBase hp = null;
 	try {
@@ -15,21 +16,18 @@ public class HelloHealpix {
 		System.exit(1);
 	}
 	
-	for (long pixtest=0; pixtest<40; pixtest++) {
+	for (long pixtest=0; pixtest<npix; pixtest++) {
 		System.out.println("For " + pixtest);
 		try {
 			long neighbors[] = hp.neighbours(pixtest);
 			for (int i=0; i < neighbors.length; i++) {
-				System.out.println("Have found neighbor " + neighbors[i]);
+				System.out.println("Have neighbor: " + neighbors[i]);
 			}
 		} catch (Exception e) {
         		System.out.println("can not get neighbours healpix");
 			System.exit(1);
 		}
 	}
-
-
-        System.out.println("Hello, World");
     }
     
 }
